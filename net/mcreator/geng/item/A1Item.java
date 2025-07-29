3b93ea0088ac938038eb0d4a4007569fd26c7166
aiLinMc <woyougemimi123@163.com>
2025-07-29 11:56:41 +0800

    Delete net directory
    I will provide a more complete source code later

--- net/mcreator/geng/item/A1Item.java
+++
@@ -1,9 +0,0 @@
-package net.mcreator.geng.item;
-
-import net.minecraft.world.item.Item;
-
-public class A1Item extends Item {
-	public A1Item() {
-		super(new Item.Properties());
-	}
-}
--- net/mcreator/geng/procedures/MelonStallGuiTxtProcedure.java
+++
@@ -1,9 +0,0 @@
-package net.mcreator.geng.procedures;
-
-import net.minecraft.network.chat.Component;
-
-public class MelonStallGuiTxtProcedure {
-	public static String execute() {
-		return Component.translatable("block.geng.melon_stall").getString();
-	}
-}
